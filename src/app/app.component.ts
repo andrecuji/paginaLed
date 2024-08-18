561186a419b2976ba32ea1d73eb58229a630b37a
andrecuji <andrescujia90@gmail.com>
2024-08-18 14:01:04 -0500

    PaginaLed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AsideComponent} from './components/layout/aside/aside.component';
+import { Aside2Component} from './components/layout/aside2/aside2.component';
+import { FooterComponent} from './components/layout/footer/footer.component';
+import { ContentComponent}  from './components/layout/content/content.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule,RouterOutlet,FooterComponent,ContentComponent,AsideComponent,Aside2Component], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/layout/aside2/aside2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aside2',
+  standalone: true,
+  imports: [],
+  templateUrl: './aside2.component.html',
+  styleUrl: './aside2.component.css'
+})
+export class Aside2Component {
+
+}
